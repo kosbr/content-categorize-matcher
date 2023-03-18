3f71128cda7ea1337b03bce1254ad8bf2672e12d
Konstantin Bryazgin <konstantin.bryazgin@diabolocom.com>
2023-03-18 21:56:20 +0200

    add main class

--- src/main/java/com/github/kosbr/matcher/CategoryMatcherApplication.java
+++ src/main/java/com/github/kosbr/matcher/CategoryMatcherApplication.java
@@ -1,6 +1,7 @@
 package com.github.kosbr.matcher;
 
 import com.github.kosbr.matcher.category.ApplicationProps;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,4 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "com.github.kosbr.matcher")
 @EnableConfigurationProperties(value = ApplicationProps.class)
 public class CategoryMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryMatcherApplication.class, args);
+    }
 }
